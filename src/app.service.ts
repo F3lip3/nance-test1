94e923435c2f24912b83ac9897771ff4645f5b56
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2022-01-18 08:26:38 -0300

    feat: structured result test

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
+export type HelloResult = { message: string };
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): HelloResult {
+    return { message: 'hello world' };
   }
 }
