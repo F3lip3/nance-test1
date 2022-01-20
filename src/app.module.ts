1f0bc56bb040f6813729d343e902f57dae3e9ccf
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2022-01-20 11:46:14 -0300

    feat: remove app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { GraphQLDateTime } from 'graphql-iso-date';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -19,8 +17,6 @@ import { UsersModule } from './users/users.module';
       }
     }),
     UsersModule
-  ],
-  controllers: [AppController],
-  providers: [AppService]
+  ]
 })
 export class AppModule {}
